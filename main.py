83dcaf30d28bbf113e0cb810b0393542e347437b
SHOP <140153886+gloryvn@users.noreply.github.com>
2026-06-04 16:08:23 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 from flask import Flask, request
 import requests
 import os
+import json
 
 app = Flask(__name__)
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
+def send_telegram(msg):
+    requests.post(
+        f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
+        json={
+            "chat_id": CHAT_ID,
+            "text": msg,
+            "parse_mode": "HTML"
+        },
+        timeout=10
+    )
 
 @app.route("/")
 def home():
@@ -11,8 +26,14 @@ def home():
 @app.route("/webhook", methods=["POST"])
 def webhook():
     data = request.get_json(silent=True)
-    print(data)
-    return "OK"
+
+    if data:
+        send_telegram(
+            "📈 <b>🏦 JPMorgan BotTrade 🤖</b>\n\n"
+            f"<pre>{json.dumps(data, indent=2)}</pre>"
+        )
+
+    return "OK", 200
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
